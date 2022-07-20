93f9776e23688c2153f7de4a0f9a4b0399ea7909
zhiwar404M <97038061+zhiwar404M@users.noreply.github.com>
2022-07-20 20:55:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ ${s2}
   
 })
 
-const channelid = "951450659981516810"//id channele vc
+const channelid = "994740237639098468"//id channele vc
 client.on("ready", () => {
     const channel = client.channels.cache.get(channelid);
     if (!channel) return console.error("The channel does not exist!");
